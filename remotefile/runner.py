eed7c1c8887966eb8963d11a0f7ca8655ea62761
Arai Hiroki <hiroara62@gmail.com>
2015-05-25 23:18:25 +0900

    print stdout / stderr and handle signal (SIGINT/SIGTERM)

--- remotefile/runner.py
+++ remotefile/runner.py
@@ -1,21 +1,29 @@
 from remotefile.remote_file import RemoteFile
 
-import logging, os, sys, shlex
+import logging, os, sys, shlex, subprocess, signal
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 class Runner(RemoteFile):
     def exec_script(self, *args):
         self.enable(force=True)
         cmd = self.__build_command(args)
-        logging.info('Now invoking `{}`.'.format(cmd))
-        exitcode = os.system(cmd)
-        if exitcode == 0:
-            logging.info('Command `{}` has been successfully completed.'.format(cmd))
+        logging.info('Now invoking `{}`.'.format(' '.join(cmd)))
+
+        p = subprocess.Popen(cmd)
+        delegate_signal = lambda signal, frame: p.send_signal(signal)
+        signal.signal(signal.SIGINT, delegate_signal)
+        signal.signal(signal.SIGTERM, delegate_signal)
+        p.wait()
+
+        if p.returncode == 0:
+            logging.info('Command `{}` has been successfully completed.'.format(' '.join(cmd)))
         else:
-            logging.info('Command `{}` has been completed with error code {}.'.format(cmd, exitcode))
-            sys.exit(exitcode)
+            logging.info('Command `{}` has been completed with error code {}.'.format(' '.join(cmd), p.returncode))
+            sys.exit(p.returncode)
 
     def __build_command(self, args):
-        return '/usr/bin/env python {} {}'.format(self.local_path, self.__build_args(args))
+        return list(filter(lambda s: len(s.strip()) > 0, ['/usr/bin/env', 'python', self.local_path, self.__build_args(args)]))
 
     def __build_args(self, args):
         return ' '.join([shlex.quote('{}'.format(arg)) for arg in args])
