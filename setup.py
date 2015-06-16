4509efcbd2a98d4ba4c735cae66e672727edaf6e
Arai Hiroki <hiroara62@gmail.com>
2015-06-16 23:45:45 +0900

    version bump to 0.0.11

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.10',
+    version='0.0.11',
 
     description='remotefile-python is wrapper of file on remote (currently supported: S3, HTTP).',
     long_description=long_description,
